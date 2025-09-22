eb92a328cb8970b02919f4b275690089dfeea33c
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 20:48:31 +0200

    Añadido Ej 10

--- UD1/10_seconds_converter/main.js
+++ UD1/10_seconds_converter/main.js
@@ -1,8 +1,24 @@
 const assert = require('assert').strict;
 
 function toHoursMinutesSeconds(value) {
+    let h = value/3600
+    let hora = Math.floor(h)
+    let m = (value%3600)/60
+    let minutos = Math.floor(m)
+    let s = (value%3600)%60
+    if (minutos<10 && s<10){
+        return hora + ":0" + minutos + ":0" + s
+    }else if(minutos<10){
+        return hora + ":0" + minutos + ":" + s
+    }else if(s<10){
+        return hora + ":" + minutos + ":0" + s
+    }else {
+        return hora + ":" + minutos + ":" + s
+    }
+    
 }
 
-assert.deepStrictEqual(toHoursMinutesSeconds(3600), "1:0:0")
-assert.deepStrictEqual(toHoursMinutesSeconds(3720), "1:20:0")
-assert.deepStrictEqual(toHoursMinutesSeconds(3725), "1:20:5")
+assert.deepStrictEqual(toHoursMinutesSeconds(3600), "1:00:00")
+assert.deepStrictEqual(toHoursMinutesSeconds(3720), "1:02:00")
+assert.deepStrictEqual(toHoursMinutesSeconds(3725), "1:02:05")
+assert.deepStrictEqual(toHoursMinutesSeconds(7540), "2:05:40")
